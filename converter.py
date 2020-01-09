ebd9dca22c2596f95792f240ba3a8c74e3075380
Samuel Ball <stmball@gmail.com>
2020-01-09 15:27:44 +0000

    first

--- converter.py
+++ converter.py
@@ -6,7 +6,7 @@ with open('outfinaltest78.csv') as file:
     output = []
     for row in data[:-1]:
         row = row.split(',')
-        output.append({'time': row[0], 'actual': row[1]})
+        output.append({'time': float(row[0]), 'actual': float(row[1])})
 
 with open('output.json', 'w') as outfile:
     json.dump(output, outfile)
