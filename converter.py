3549972703df29cab8399169c6e2fd7a3ca5c0e2
Samuel Ball <stmball@gmail.com>
2020-01-10 10:58:17 +0000

    output4

--- converter.py
+++ converter.py
@@ -1,12 +1,12 @@
 import json
 
-with open('outfinaltest78.csv') as file:
+with open('cts_data_6.csv') as file:
     data = file.read()
-    data = data.split('\n')
+    data = data.split('\n')[:100000]
     output = []
     for row in data[:-1]:
         row = row.split(',')
-        output.append({'time': float(row[0]), 'actual': float(row[1]), 'predicted': int(row[2])})
+        output.append({'time': float(row[3]), 'actual': float(row[4]), 'predicted': float(row[2])})
 
-with open('output3.json', 'w') as outfile:
+with open('output4.json', 'w') as outfile:
     json.dump(output, outfile)
