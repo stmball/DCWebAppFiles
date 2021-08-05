ccd3d813d6bf435243b72f7f1ab50f8185b1ee7e
Samuel Ball <stmball@gmail.com>
2021-08-05 13:29:12 +0100

    help

--- converter.py
+++ converter.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def convert(dataset):
     data = pd.read_csv(dataset)
-    newdata = data[["Time", "Noisy Current"]].to_numpy()
-    np.savetxt(dataset, newdata, fmt='%f', delimiter=",")
+    newdata = data[["Time", "Noisy Current"]][:100000].to_numpy()
+    np.savetxt(f'{dataset}_new', newdata, fmt='%f', delimiter=",")
     return 1
 
-print(convert("./Datasets/ten_channel_simulated"))
+print(convert("./Datasets/single_channel.csv"))
